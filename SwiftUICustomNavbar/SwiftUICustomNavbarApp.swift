48fbf33f1d5622770c90f8708b320f9e8b64ddc8
Srinivasan Rajendran <srinivasan.rajendran@truecaller.comm>
2022-02-13 12:52:20 +0100

    Initial Commit

---
+++ SwiftUICustomNavbar/SwiftUICustomNavbarApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICustomNavbarApp.swift
+//  SwiftUICustomNavbar
+//
+//  Created by Srinivasan Rajendran on 2022-02-13.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICustomNavbarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
